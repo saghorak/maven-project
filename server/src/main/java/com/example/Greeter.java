1074fde1b937e0a3c28fb951660174990d04083b
mtripepi <mtripepi-ZVRS@mtripepis-MacBook-Pro.local>
2017-06-01 23:43:23 -0400

    fix checkstyle warnings 3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   * This is a javadoc comments.
+   * @param someone is the name of a person
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
